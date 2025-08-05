fc993bd628af2106ce0ae5018a56b54ba2918dbe
smahamud3 <sultan52093@gmail.com>
2025-08-05 08:51:16 -0400

    'system information part'

--- sysinfoA2.py
+++ sysinfoA2.py
@@ -3,23 +3,26 @@
 Author: Sultan Mahamud
 """
 
-import os
-import sys
+import os   #we are importing the os module so we can run  commands like 'uname', 'df', 'free' from Python using os.popen()
+import sys  #sys is needed to access command line arguments passed to the script
 
-def get_system_info():
-    info = []
-#Systen' hostname 
-    hostname = os.popen('hostname').read().strip()
-    info.append("Hostname: " + hostname)
+
+def get_system_info():  #function to gather general system information
+
+    info = []  #this command will store all the system info messages in this info list and return it at the end
+    hostname = os.popen('hostname').read().strip() #'hostname' command returns the name of the computer.we read and strip the newline character
+    info.append("Hostname: " + hostname)    #adding hostname string to the list
+
 #OS information
-    os_info = os.popen('uname -o').read().strip()
-    info.append("Operating System: " + os_info)
+    os_info = os.popen('uname -o').read().strip()  #'uname -o' returns the operating system type
+    info.append("Operating System: " + os_info)  #adding OS type to the list
 #Kernel Version information
-    kernel = os.popen('uname -r').read().strip()
-    info.append("Kernel Version: " + kernel)
+    kernel = os.popen('uname -r').read().strip() #'uname -r' gives the kernel version the OS is currently using
+    info.append("Kernel Version: " + kernel)  #adding kernel version string to the list.
 
-    uptime = os.popen('uptime -p').read().strip()
-    info.append("Uptime: " + uptime)
+    uptime = os.popen('uptime -p').read().strip() #'uptime -p' returns system uptime in a more human readable format like "up 2 hours, 5 minutes"
+    info.append("Uptime: " + uptime)  #adding uptime string to the list
 
-    return info
+
+    return info #it returns the info list so the calling function can print it or use it later
 
